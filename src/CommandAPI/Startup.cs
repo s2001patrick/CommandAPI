2130ae6a87c77374d8393ab73c51e5c2ba10bc78
Patrick Yeung <s2001patrick@gmail.com>
2022-02-19 15:49:18 +0800

    Added Data Transfer Objects

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
+using AutoMapper;
 
 namespace CommandAPI
 {
@@ -33,6 +34,8 @@ namespace CommandAPI
 
             services.AddDbContext<CommandContext>(opt => opt.UseNpgsql (builder.ConnectionString));
             services.AddControllers();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             //services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
             services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
